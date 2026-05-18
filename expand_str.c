46a91bccb6c17c827797f5d29c8d42108042bce1
gblas-he <gblas-he@student.42madrid.com>
2026-05-18 17:14:18 +0200

    exv5

---
+++ expand_str.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+int main (int ac, char **av)
+{
+	int i;
+	int first_word = 1;
+
+	if (ac == 2)
+	{
+		i = 0;
+		while (av[1][i] == ' ' || av[1][i] == '\t')
+			i++;
+		while (av[1][i])
+		{
+			while (av[1][i] == ' ' || av[1][i] == '\t' &&)
+				write(1, "   ", 3);
+			write (1, &av[1][i], 1);
+			i++;
+		}
+	}
+	write (1, "\n", 1);
+	return (0);
+}
