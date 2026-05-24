1490cb90432f90d652480d9754c587bf4df236bc
gblas-he <gblas-he@student.42madrid.com>
2026-05-24 18:20:04 +0200

    exv9

--- ft_split.c
+++ ft_split.c
@@ -2,72 +2,99 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char **ft_words(char **arr, char *s)
+int	ft_wordlen(char *str)
 {
 	int i = 0;
-	int words = 0;
 
-    while (s[i])
-    {
-        // Saltar espacios
-        while (s[i] == ' ')
-            i++;
-        // Si no es el final, es una palabra
-        if (s[i])
-            words++;
-        // Saltar la palabra
-        while (s[i] && s[i] != ' ')
-            i++;
-    }
-	return (words);
+	while (str[i] != '\0' && str[i] != ' ' && str[i] != '\t' && str[i] != '\n')
+		++i;
+	return (i);
 }
 
-int	ft_strwordlen(char *s)
+char **fill_words(char **arr, char *s)
 {
 	int i = 0;
-	int words = 0;
+	int j;
+	int len;
+	int word_index = 0;
 
-    while (s[i])
-    {
-        // Saltar espacios
-        while (s[i] == ' ')
-            i++;
-        // Si no es el final, es una palabra
-        if (s[i])
-            words++;
-        // Saltar la palabra
-        while (s[i] && s[i] != ' ')
-            i++;
-    }
-	return (words);
+	while (s[i])
+	{
+		// Saltar espacios
+		while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+			i++;
+
+		if (s[i])
+		{
+			len = ft_wordlen(&s[i]);
+			arr[word_index] = malloc(sizeof(char) * (len + 1));
+			if (!arr[word_index])
+				return (NULL);
+			j = 0;
+			while (j < len)
+			{
+				arr[word_index][j] = s[i + j];
+				j++;
+			}
+			arr[word_index][j] = '\0';
+			word_index++;
+
+			// Saltar palabra
+			while (s[i] != '\0' && s[i] != ' '
+				&& s[i] != '\t' && s[i] != '\n')
+				i++;
+		}
+	}
+	return (arr);
+}
+
+int	count_words(char *s)
+{
+	int i = 0;
+	int num_words = 0;
+
+	while (s[i])
+	{
+		while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+			i++;
+
+		// Si hay palabra
+		if (s[i])
+			num_words++;
+
+		// Saltar palabra
+		while (s[i] && s[i] != ' '
+			&& s[i] != '\t' && s[i] != '\n')
+			i++;
+	}
+	return (num_words);
 }
 
 char    **ft_split(char *str)
 {
-	int i = 0;
-	int words = 0;
+	int num_words = 0;
 	char **arr;
 
-	words = ft_strwordlen(str);
-	arr = (char **)malloc(words + 1);
+	num_words = count_words(str);
+	arr = (char **)malloc(sizeof(char *) * (num_words + 1));
 	if (!arr)
-		return NULL;
-	arr[words] = 0;
-	arr = ft_words(arr, str);
+		return (NULL);
+	arr[num_words] = 0;
+	arr = fill_words(arr, str);
 	return (arr);
 }
 
-int main (void)
+int main(void)
 {
 	int i = 0;
 	char **s = ft_split("  Hello world   welcome to the jungle  ");
 
-    while (s[i])
-    {
-        printf("%s\n", s[i]);
-        free(s[i]);
-        i++;
-    }
-    free(s);
+	while (s[i])
+	{
+		printf("%s\n", s[i]);
+		free(s[i]);
+		i++;
+	}
+	free(s);
 	return (0);
 }
