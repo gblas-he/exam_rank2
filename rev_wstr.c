54f53d1b622b4de7939002aced266e79d291c0d5
gblas-he <gblas-he@student.42madrid.com>
2026-05-29 18:09:18 +0200

    exv16

--- rev_wstr.c
+++ rev_wstr.c
@@ -1,56 +1,41 @@
-
 #include <unistd.h>
 
-void ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-int ft_strlen(char *str)
-{
-	int i = 0;
-	
-	while (str[i])
-		i++;
-	return (i);
-}
-
 // Invierte el orden de las palabras en una cadena e imprime el resultado palabra por palabra.
-int main(int ac, char *av[])
+void	rev_wstr(char *s)
 {
-	char *tmp;
-	char *rev;
-	int len;
+	int	i = 0;
+	int	first = 1;
 
-	if (ac == 2)
+	while (s[i])
+		i++;
+	i--;
+
+	while (i >= 0)
 	{
-		tmp = av[1];
-		len = ft_strlen(tmp); // Calcula longitud
-		rev = NULL;
-		len--;
-		while (tmp[len]) // Recorre desde el final
+		while (i >= 0 && (s[i] == ' ' || s[i] == '\t' || s[i] == '\n'))
+			i--;
+		// por que recorrer desde el principio puede a ver espacios
+		// capturamos el caracter final
+		int end = i;
+		while (i >= 0 && s[i] != ' ' && s[i] != '\t' && s[i] != '\n')
+			i--;
+		int start = i + 1;
+		// si no es lapriemra palabra imprime espacio si no quedaria " hola mundo"
+		if (!first)
+			write(1, " ", 1);
+		//ponemos un limite en la palabra para que no se rompa el bucle
+		while (start <= end)
 		{
-			if (tmp[len - 1] == ' ') // Si encuentra un espacio antes del carácter actual
-			{
-				rev = &tmp[len]; // rev apunta al inicio de la palabra
-				while (*rev && *rev != ' ')
-				{
-					ft_putchar(*rev);
-					rev++;
-				}
-				ft_putchar(' ');
-			}
-			else if (len == 0) // Si llegó al inicio (primera palabra
-			{
-				rev = &tmp[len]; // rev apunta al inicio de la primera palabra
-				while (*rev && *rev != ' ')
-				{
-					ft_putchar(*rev);
-					rev++;
-				}
-			}
-			len--;
+			write(1, &s[start], 1);
+			start++;
 		}
+		first = 0;
 	}
-	ft_putchar('\n');
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+		rev_wstr(av[1]);
+	write(1, "\n", 1);
 }
--- rostring.c
+++ rostring.c
@@ -1,66 +1,58 @@
-
 #include <unistd.h>
 
-// Salta espacios y tabs desde la posición i
-int		skip_whitespace(char *str, int i)
+// Mueve la primera palabra al final de la string
+void	rostring(char *s)
 {
-	while (str[i] == ' ' || str[i] == '\t')
-		++i;
-	return (i);
-}
+	int	i = 0;
+	int	start;
+	int	end;
+	int	flag = 0;
 
-int		ft_wordlen(char *str)
-{
-	int i = 0;
+	// guardar primera palabra
+	while (s[i] == ' ' || s[i] == '\t')
+		i++;
 
-	while (str[i] != '\0' && str[i] != ' ' && str[i] != '\t')
-		++i;
-	return (i);
-}
+	start = i;
 
-// Elimina espacios y tabulaciones extra de una cadena, imprime las palabras separadas por un solo espacio y mueve la primera palabra al final.// Imprime una palabra con espacio delante (excepto la primera)
-int		print_word(char *str, int i, int *is_first)
-{
-	int word_len; 
+	while (s[i] && s[i] != ' ' && s[i] != '\t')
+		i++;
 
-	i = skip_whitespace(str, i); // Salta espacios antes de la palabra
-	word_len = ft_wordlen(str + i); // Calcula longitud de la palabra
-	if (*is_first == 0)
+	end = i;
+
+	// imprimir resto
+	while (s[i])
+	{
+		while (s[i] == ' ' || s[i] == '\t')
+			i++;
+
+		if (s[i] && flag)
+			write(1, " ", 1);
+
+		while (s[i] && s[i] != ' ' && s[i] != '\t')
+		{
+			write(1, &s[i], 1);
+			i++;
+			flag = 1;
+		}
+	}
+
+	// imprimir espacio antes de la primera palabra
+	if (flag)
 		write(1, " ", 1);
-	write(1, str + i, word_len);
-	*is_first = 0;
-	return (i + word_len);
+
+	// imprimir primera palabra
+	while (start < end)
+	{
+		write(1, &s[start], 1);
+		start++;
+	}
 }
 
-// Imprime todas las palabras desde la posición i en adelante
-int		epur_str(char *str)
+int	main(int ac, char **av)
 {
-	int i = 0;
-	int is_first = 1;
+	if (ac > 1)
+		rostring(av[1]);
 
-	i = skip_whitespace(str, i);
-	while (str[i] != '\0')
-	{
-		i = print_word(str, i, &is_first);
-		i = skip_whitespace(str, i);
-	}
-	return (is_first);
-}
-
-// El programa elimina espacios extra y mueve la primera palabra al final de la cadena.
-int		main(int argc, char **argv)
-{
-	if (argc >= 2)
-	{
-		char *str = argv[1];
-		int i = 0;
-		int is_first;
-
-		i = skip_whitespace(str, i);
-		i = i + ft_wordlen(str + i);
-		is_first = epur_str(str + i);
-		print_word(str, 0, &is_first);
-	}
 	write(1, "\n", 1);
 	return (0);
 }
