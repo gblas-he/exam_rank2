c8c4e6e19668724f988bae0b300e77bb99ebb6e3
gblas-he <gblas-he@student.42madrid.com>
2026-05-29 20:17:45 +0200

    exv16

--- flood_fill.c
+++ flood_fill.c
@@ -1,24 +1,24 @@
-
 typedef struct 	s_point {
 	int			x;				// x : Width  | x-axis
 	int			y;				// y : Height | y-axis
 }				t_point;
- 
+
 // Implementa un algoritmo de flood fill (relleno por inundación) que reemplaza una región conectada de un mismo valor por 'F' en una matriz.
-void	fill(char **tab, t_point size, t_point cur, char to_fill)
+void	fill(char **tab, t_point size, int x, int y, char to_fill)
 {
-	if (cur.y < 0 || cur.y >= size.y || cur.x < 0 || cur.x >= size.x
-		|| tab[cur.y][cur.x] != to_fill)
+	if (y < 0 || y >= size.y || x < 0 || x >= size.x
+		|| tab[y][x] != to_fill)
 		return;
 
-	tab[cur.y][cur.x] = 'F';
-	fill(tab, size, (t_point){cur.x - 1, cur.y}, to_fill);
-	fill(tab, size, (t_point){cur.x + 1, cur.y}, to_fill);
-	fill(tab, size, (t_point){cur.x, cur.y - 1}, to_fill);
-	fill(tab, size, (t_point){cur.x, cur.y + 1}, to_fill);
+	tab[y][x] = 'F';
+	fill(tab, size, x - 1, y, to_fill);
+	fill(tab, size, x + 1, y, to_fill);
+	fill(tab, size, x, y - 1, to_fill);
+	fill(tab, size, x, y + 1, to_fill);
 }
 
 void	flood_fill(char **tab, t_point size, t_point begin)
 {
-	fill(tab, size, begin, tab[begin.y][begin.x]);
+	fill(tab, size, begin.x, begin.y, tab[begin.y][begin.x]);
 }
+
