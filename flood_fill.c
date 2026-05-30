a87946faf135c9c1b88ad06027a64e95535a95a0
gblas-he <gblas-he@student.42madrid.com>
2026-05-30 18:53:52 +0200

    exv21

--- flood_fill.c
+++ flood_fill.c
@@ -2,21 +2,18 @@
 #include <stdlib.h>
 
 typedef struct 	s_point {
-	int			x;				// x : Width  | x-axis
-	int			y;				// y : Height | y-axis
+	int			x;				// x : ancho o columnas  | x-axis
+	int			y;				// y : altura o filas | y-axis
 }				t_point;
 
 // Implementa un algoritmo de flood fill (relleno por inundación) que reemplaza una región conectada de un mismo valor por 'F' en una matriz.
 void	fill(char **tab, t_point size, int x, int y, char to_fill)
 {
-	// Caso base:
-	// - fuera de los límites de la matriz
-	// - o el carácter actual no coincide con el objetivo a reemplazar
-	if (y < 0 || y >= size.y || x < 0 || x >= size.x
-		|| tab[y][x] != to_fill)
+	if (y < 0 || y >= size.y || x < 0 || x >= size.x // se comprueba fuera de los límites de la matriz
+		|| tab[y][x] != to_fill) // to_fill: el carácter actual no coincide con el objetivo a reemplazar y continua el código
 		return;
 
-	tab[y][x] = 'F';
+	tab[y][x] = 'F'; // se reemplaza la posicion por 'F' esto es solo si to_fill = tab[begin.y][begin.x] si no no llega a esta parte del código
 
 	// Llamadas recursivas en las 4 direcciones (arriba, abajo, izquierda, derecha)
 	fill(tab, size, x - 1, y, to_fill); // izquierda
@@ -29,10 +26,13 @@ void	fill(char **tab, t_point size, int x, int y, char to_fill)
 void	flood_fill(char **tab, t_point size, t_point begin)
 {
 	// Guarda el carácter inicial que se quiere reemplazar
-	fill(tab, size, begin.x, begin.y, tab[begin.y][begin.x]);
+	fill(tab, size, begin.x, begin.y, tab[begin.y][begin.x]); // tab[begin.y][begin.x] indica  el contenido que hay en esa posición.
 }
 
 // Devuelve una copia dinámica y mutable de zone
+/*     DEDUCCIÓN DE FILAS Y COLUMNAS (size.x / size.y)
+        new = malloc(sizeof(char*) * size.y); filas o alto
+        new[i] = malloc(size.x); columnas o ancho*/ 
 char**  make_area(char **zone, t_point size)
 {
     char **new;
