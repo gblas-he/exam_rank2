1ff20741d37fb382119ad0b9c0cbb6a2742829d7
Giovanni Blas Heredia <gblas-he@c2r11s6.42madrid.com>
2026-05-30 16:40:23 +0200

    exv20

--- flood_fill.c
+++ flood_fill.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct 	s_point {
 	int			x;				// x : Width  | x-axis
 	int			y;				// y : Height | y-axis
@@ -29,3 +32,41 @@ void	flood_fill(char **tab, t_point size, t_point begin)
 	fill(tab, size, begin.x, begin.y, tab[begin.y][begin.x]);
 }
 
+// Devuelve una copia dinámica y mutable de zone
+char**  make_area(char **zone, t_point size)
+{
+    char **new;
+
+    new = malloc(sizeof(char *) * size.y); // array de filas
+    for (int i = 0; i < size.y; ++i)
+    {
+        new[i] = malloc(size.x + 1);       // reserva fila + '\0'
+        for (int j = 0; j < size.x; ++j)
+            new[i][j] = zone[i][j];         // copia carácter a carácter
+        new[i][size.x] = '\0';
+    }
+    return new;
+}
+
+int main(void)
+{
+    t_point size = {8, 5};
+    char *zone[] = {                        // matriz original (read-only)
+        "11111111",
+        "10001001",
+        "10010001",
+        "10110001",
+        "11100001",
+    };
+
+    char **area = make_area(zone, size);    // copia mutable de zone
+    for (int i = 0; i < size.y; ++i)
+        printf("%s\n", area[i]);            // imprime matriz inicial
+    printf("\n");
+
+    t_point begin = {7, 4};                 // punto de inicio (esquina inferior derecha)
+    flood_fill(area, size, begin);
+    for (int i = 0; i < size.y; ++i)
+        printf("%s\n", area[i]);            // imprime resultado
+    return (0);
+}
