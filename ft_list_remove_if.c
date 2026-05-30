8af9976ba4be3834e2e7afcdd7e09eb95b86b049
Giovanni Blas Heredia <gblas-he@c2r11s6.42madrid.com>
2026-05-30 14:45:35 +0200

    exv17

--- ft_list_remove_if.c
+++ ft_list_remove_if.c
@@ -14,7 +14,8 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
     if (!begin_list || !*begin_list)
         return;
 
-    // Primero eliminar nodos del inicio si coinciden
+    // Queremos eliminar el nodo 1: begin_list → [nodo1] → [nodo2] → [nodo3] → NULL .Usamos *begin_list para eliminar la cabeza porque es el puntero real.
+    // Primero eliminar nodos si coinciden en el inicio
     while (*begin_list && cmp((*begin_list)->data, data_ref) == 0)
     {
         tmp = *begin_list;
@@ -23,8 +24,9 @@ void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
     }
 
     current = *begin_list;
-
-    // Recorrer el resto de la lista
+    
+    // Queremos eliminar en nodo 2: begin_list → [nodo1] → [nodo2] → [nodo3] → NULL .Usamos current para el resto porque solo redirigimos current->next. 
+    // si no coinciden en el inicio. Recorrer el resto de la lista
     while (current && current->next)
     {
         if (cmp(current->next->data, data_ref) == 0)
