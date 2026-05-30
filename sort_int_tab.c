53da22ff34fb1036f6ad5a9026ff09a2d39b2b5e
Giovanni Blas Heredia <gblas-he@c2r11s6.42madrid.com>
2026-05-30 15:31:31 +0200

    exv19

--- sort_int_tab.c
+++ sort_int_tab.c
@@ -1,41 +1,23 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   sort_int_tab.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: gblas-he <gblas-he@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/25 13:31:03 by gblas-he          #+#    #+#             */
-/*   Updated: 2026/04/25 13:31:39 by gblas-he         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <unistd.h>
 #include <stdio.h>
 
-void sort_int_tab(int *tab, unsigned int size)
+void	sort_int_tab(int *tab, unsigned int size)
 {
-	unsigned int i;
-	unsigned int j;
-	int tmp;
+	unsigned int	i = 0;
+	int	temp;
 
-
-	i = 0;
-	while(i < size)
+	// Recorre el array hasta que no haya intercambios. si el tamaño es 3 seria -1 porque 0,1,2
+	while (i < (size - 1))
 	{
-		j = 0;
-		tmp = 0;
-		while(j < size - 1 - i)
-		{			
-			if (tab[j] > tab[j + 1])
-			{
-				tmp = tab[j];
-				tab[j] = tab[j + 1];
-				tab[j + 1] = tmp;
-			}
-			j++;
+		// Si el elemento actual es mayor que el siguiente, los intercambia
+		if (tab[i] > tab[i + 1])
+		{
+			temp = tab[i];
+			tab[i] = tab[i+ 1];
+			tab[i + 1] = temp;
+			i = 0; // Reinicia desde el principio tras cada intercambio
 		}
-		i++;
+		else
+			i++;
 	}
 }
 
@@ -53,3 +35,15 @@ int main (void)
 	}
 	return(0);
 }
+
+/* Array inicial: [5, 6, 2, 10]
+
+i=0: 5 > 6 → no → i=1
+i=1: 6 > 2 → intercambia → [5, 2, 6, 10], i=0
+i=0: 5 > 2 → intercambia → [2, 5, 6, 10], i=0
+i=0: 2 > 5 → no → i=1
+i=1: 5 > 6 → no → i=2
+i=2: 6 > 10 → no → i=3
+i=3: 3 < 3 → fin del while
+
+Array final: [2, 5, 6, 10] */
